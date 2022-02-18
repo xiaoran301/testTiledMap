d94b730c80401fcb97da0fc5143fdb0d395b8f5e
hengjin.xu <xiaoran301@gmail.com>
2022-02-18 18:16:37 +0800

    overhead test

---
+++ Project/Assets/Scripts/SwapPositionYAndZ.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapPositionYAndZ : MonoBehaviour
+{
+    [SerializeField] private GameObject _targetGO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+#if UNITY_EDITOR
+
+    void OnValidate()
+    {
+        if (_targetGO != null)
+        {
+            Vector3 oldPos;
+            var transforms = _targetGO.GetComponentsInChildren<Transform>();
+            foreach (var transform in transforms)
+            {
+                oldPos = transform.localPosition;
+                transform.localPosition.Set(oldPos.x, oldPos.z, oldPos.y);
+            }
+
+            oldPos = _targetGO.transform.localPosition;
+            _targetGO.transform.localPosition.Set(oldPos.x, oldPos.z, oldPos.y);
+        }
+    }
+
+#endif
+}
---
+++ Project/Assets/Scripts/UnityVector3Extension.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace ExtensionMethods
+{
+    public static class Vector3Extension
+    {
+        public static Vector2 GetXZ(this Vector3 pos)
+        {
+            return new Vector2(pos.x, pos.z);
+        }
+
+        public static void SetXZ(this Vector3 pos, Vector2 pos2)
+        {
+            pos.x = pos2.x;
+            pos.z = pos2.y;
+        }
+    }
+}
